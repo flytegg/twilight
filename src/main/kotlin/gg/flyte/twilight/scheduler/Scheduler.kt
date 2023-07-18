bd6b24b70f999c0f4acc2b267976aaf57dd31e7b
Josh <jshbker@gmail.com>
2023-07-18 03:09:18 +0100

    Add every scheduler param combination under the sun (that makes sense)

--- src/main/kotlin/gg/flyte/twilight/scheduler/Scheduler.kt
+++ src/main/kotlin/gg/flyte/twilight/scheduler/Scheduler.kt
@@ -3,6 +3,7 @@ package gg.flyte.twilight.scheduler
 import gg.flyte.twilight.Twilight
 import org.bukkit.scheduler.BukkitRunnable
 import org.bukkit.scheduler.BukkitTask
+import sun.jvm.hotspot.oops.CellTypeState.value
 import java.util.concurrent.TimeUnit
 
 fun sync(runnable: BukkitRunnable.() -> Unit): BukkitTask {
@@ -13,7 +14,7 @@ fun async(runnable: BukkitRunnable.() -> Unit): BukkitTask {
     return createBukkitRunnable(runnable).runTaskAsynchronously(Twilight.plugin)
 }
 
-fun delay(value: Int, async: Boolean = false, unit: TimeUnit = TimeUnit.MILLISECONDS, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+fun delay(value: Int, unit: TimeUnit = TimeUnit.MILLISECONDS, async: Boolean = false, runnable: BukkitRunnable.() -> Unit): BukkitTask {
     return if (async) {
         createBukkitRunnable(runnable).runTaskLaterAsynchronously(Twilight.plugin, unit.toMillis(value.toLong()) / 50)
     } else {
@@ -21,11 +22,15 @@ fun delay(value: Int, async: Boolean = false, unit: TimeUnit = TimeUnit.MILLISEC
     }
 }
 
-fun delay(value: Int, unit: TimeUnit = TimeUnit.MILLISECONDS, async: Boolean = false, runnable: BukkitRunnable.() -> Unit): BukkitTask {
-    return delay(value, async, unit, runnable)
+fun delay(ticks: Int = 1, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return delay(ticks, TimeUnit.MILLISECONDS, false, runnable)
 }
 
-fun repeat(delay: Int, period: Int, async: Boolean = false, unit: TimeUnit = TimeUnit.MILLISECONDS, runnable: Runnable.() -> Unit): BukkitTask {
+fun delay(ticks: Int = 1, async: Boolean, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return delay(ticks, TimeUnit.MILLISECONDS, async, runnable)
+}
+
+fun repeat(delay: Int, period: Int, unit: TimeUnit = TimeUnit.MILLISECONDS, async: Boolean = false, runnable: BukkitRunnable.() -> Unit): BukkitTask {
     return if (async) {
         createBukkitRunnable(runnable).runTaskTimerAsynchronously(Twilight.plugin, unit.toMillis(delay.toLong()) / 50, unit.toMillis(period.toLong()) / 50)
     } else {
@@ -33,11 +38,31 @@ fun repeat(delay: Int, period: Int, async: Boolean = false, unit: TimeUnit = Tim
     }
 }
 
-fun repeat(delay: Int, period: Int, unit: TimeUnit = TimeUnit.MILLISECONDS, async: Boolean = false, runnable: Runnable.() -> Unit): BukkitTask {
-    return repeat(delay, period, async, unit, runnable)
+fun repeat(period: Int, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return repeat(period, period, TimeUnit.MILLISECONDS, false, runnable)
 }
 
-fun createBukkitRunnable(runnable: BukkitRunnable.() -> Unit): BukkitRunnable {
+fun repeat(period: Int, async: Boolean, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return repeat(period, period, TimeUnit.MILLISECONDS, async, runnable)
+}
+
+fun repeat(period: Int, unit: TimeUnit, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return repeat(period, period, unit, false, runnable)
+}
+
+fun repeat(period: Int, unit: TimeUnit, async: Boolean, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return repeat(period, period, unit, async, runnable)
+}
+
+fun repeat(delay: Int, period: Int, async: Boolean, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return repeat(delay, period, TimeUnit.MILLISECONDS, async, runnable)
+}
+
+fun repeat(delay: Int, period: Int, unit: TimeUnit, runnable: BukkitRunnable.() -> Unit): BukkitTask {
+    return repeat(delay, period, unit, false, runnable)
+}
+
+private fun createBukkitRunnable(runnable: BukkitRunnable.() -> Unit): BukkitRunnable {
     return object : BukkitRunnable() {
         override fun run() {
             this.runnable()
