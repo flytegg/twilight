c9ed2e07e90e34043b5a8f993746cfa2725aa6e7
Josh <jshbker@gmail.com>
2023-07-18 01:57:30 +0100

    Change to LivingEntity and use new attributes over maxHealth

--- src/main/kotlin/gg/flyte/twilight/extension/Entity.kt
+++ src/main/kotlin/gg/flyte/twilight/extension/Entity.kt
@@ -8,10 +8,6 @@ fun Entity.getNearbyEntities(range: Double): MutableList<Entity> {
     return getNearbyEntities(range, range, range)
 }
 
-fun Player.isOnLadder(): Boolean {
-    return location.block.type == Material.LADDER
-}
-
 fun Entity.isOnFire(): Boolean {
     return fireTicks > 0
 }
