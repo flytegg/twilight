8585ce9b9b20feca5c5dfe040acc9fbd9057b85e
Josh <jshbker@gmail.com>
2023-07-18 02:02:18 +0100

    Cleanup code & imports

--- src/main/kotlin/gg/flyte/twilight/event/entity/EntityEvent.kt
+++ src/main/kotlin/gg/flyte/twilight/event/entity/EntityEvent.kt
@@ -2,6 +2,5 @@ package gg.flyte.twilight.event.entity
 
 import gg.flyte.twilight.event.Event
 import org.bukkit.entity.Entity
-import org.bukkit.entity.Player
 
 open class EntityEvent(val entity: Entity) : Event()
