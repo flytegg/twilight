b2733362f23c6cb4a56470b99e2f5f37310a0eea
Josh <jshbker@gmail.com>
2023-07-18 01:58:09 +0100

    Move events

--- src/main/kotlin/gg/flyte/twilight/event/player/PlayerEvent.kt
+++ src/main/kotlin/gg/flyte/twilight/event/player/PlayerEvent.kt
@@ -1,5 +1,6 @@
-package gg.flyte.twilight.event
+package gg.flyte.twilight.event.player
 
+import gg.flyte.twilight.event.Event
 import org.bukkit.entity.Player
 
 open class PlayerEvent(val player: Player) : Event()
