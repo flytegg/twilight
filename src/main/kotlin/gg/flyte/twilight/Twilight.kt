4885fd6c3f2f2232fa9247f36a52d07c54bba628
Josh <jshbker@gmail.com>
2023-07-18 05:20:50 +0100

    Fix env implementation for builder

--- src/main/kotlin/gg/flyte/twilight/Twilight.kt
+++ src/main/kotlin/gg/flyte/twilight/Twilight.kt
@@ -1,21 +1,17 @@
 package gg.flyte.twilight
 
+import gg.flyte.twilight.environment.Environment
 import gg.flyte.twilight.event.CustomEventRegistry
 import gg.flyte.twilight.inventory.GUIListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Twilight(javaPlugin: JavaPlugin) {
-    lateinit var customEvents: CustomEventRegistry
-    var property = ""
+
+    private lateinit var customEvents: CustomEventRegistry
 
     init {
         plugin = javaPlugin
-        //Environment.env(envSettings)
-
-        /**
-         * Register event listeners
-         */
 
         listOf(
             GUIListener()
@@ -26,21 +22,13 @@ class Twilight(javaPlugin: JavaPlugin) {
         lateinit var plugin: JavaPlugin
     }
 
-    fun events(init: CustomEventRegistry.() -> Unit): CustomEventRegistry {
-        val customEvents = CustomEventRegistry.apply(init)
-        this.customEvents = customEvents
-        return customEvents
+    fun env(init: Environment.Settings.() -> Unit) {
+        Environment.env(Environment.Settings().apply(init))
     }
 
-    /*class Builder(private val plugin: JavaPlugin) {
-        private var envBuilder = Environment.Builder()
-
-        fun env(envBuilder: Environment.Builder.() -> Unit) = apply {
-            this.envBuilder.envBuilder()
-        }
-
-        fun build() = Twilight(plugin, envBuilder.build())
-    }*/
+    fun events(init: CustomEventRegistry.() -> Unit) {
+        this.customEvents = CustomEventRegistry.apply(init)
+    }
 
 }
 
