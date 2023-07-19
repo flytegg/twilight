0987a9358a78d5894b0e3fd960a58b194c432209
Josh <jshbker@gmail.com>
2023-07-19 02:53:28 +0100

    Add Map extension for getting all keys by value

--- src/main/kotlin/gg/flyte/twilight/extension/Map.kt
+++ src/main/kotlin/gg/flyte/twilight/extension/Map.kt
@@ -3,3 +3,7 @@ package gg.flyte.twilight.extension
 fun <K, V> Map<K, V>.findKeyByValue(value: V): K? {
     return entries.find { it.value == value }?.key
 }
+
+fun <K, V> Map<K, V>.findKeysByValue(value: V): List<K> {
+    return entries.filter { it.value == value }.map { it.key }
+}
