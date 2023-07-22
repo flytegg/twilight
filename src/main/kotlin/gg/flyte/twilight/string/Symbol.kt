88973e2abf16a9167c0e3a6b72a99e9507ec85f7
Stephen <138153996+stephendotgg@users.noreply.github.com>
2023-07-22 01:24:52 +0100

    Symbols

---
+++ src/main/kotlin/gg/flyte/twilight/string/Symbol.kt
@@ -0,0 +1,23 @@
+package gg.flyte.twilight.string
+
+object Symbol {
+
+    const val BULLET = "•"
+
+    const val HEART = "❤"
+
+    const val STAR = "★"
+
+    const val SMALL_RIGHT = "»"
+
+    const val SQUARE = "█"
+
+    const val CIRCLE = "⬤"
+
+    const val PICKAXE = "⛏"
+
+    const val COIN = "⛀"
+
+    const val COINS = "⛁"
+
+}
