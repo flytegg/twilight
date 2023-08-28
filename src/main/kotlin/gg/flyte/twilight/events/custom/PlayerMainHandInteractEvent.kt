6f6aad80d9b05e93a2e9b1ea716517d3c61817e7
stephendotgg <138625055+stephendotgg@users.noreply.github.com>
2023-08-28 07:55:02 +0100

    Event

--- src/main/kotlin/gg/flyte/twilight/events/custom/PlayerMainHandInteractEvent.kt
+++ src/main/kotlin/gg/flyte/twilight/events/custom/PlayerMainHandInteractEvent.kt
@@ -3,8 +3,6 @@ package gg.flyte.twilight.events.custom
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
-import org.bukkit.event.Cancellable
-import org.bukkit.event.HandlerList
 import org.bukkit.event.block.Action
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
@@ -30,49 +28,4 @@ class PlayerMainHandInteractEvent(
     item,
     clickedBlock,
     clickedFace
-), Cancellable {
-
-    private var cancelled = false
-
-    /**
-     * Checks if the event is cancelled.
-     *
-     * @return True if the event is cancelled, false otherwise.
-     */
-    override fun isCancelled(): Boolean {
-        return cancelled
-    }
-
-    /**
-     * Sets the cancelled status of the event.
-     *
-     * @param cancel True to cancel the event, false to allow it.
-     */
-    override fun setCancelled(cancel: Boolean) {
-        cancelled = cancel
-    }
-
-    companion object {
-        val HANDLERS = HandlerList()
-
-        /**
-         * Returns the list of event handlers for this event.
-         *
-         * @return The HandlerList containing the event handlers.
-         */
-        @JvmStatic
-        fun getHandlerList(): HandlerList {
-            return HANDLERS
-        }
-    }
-
-    /**
-     * Returns the list of event handlers for this event.
-     *
-     * @return The HandlerList containing the event handlers.
-     */
-    override fun getHandlers(): HandlerList {
-        return HANDLERS
-    }
-
-}
+)
