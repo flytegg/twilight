30c4c6b8748dae2a3645d2b8c0f00a5be9bfbf08
Stephen <138153996+stephendotgg@users.noreply.github.com>
2023-09-02 01:18:53 +0100

    More extensions

--- src/main/kotlin/gg/flyte/twilight/gson/Gson.kt
+++ src/main/kotlin/gg/flyte/twilight/gson/Gson.kt
@@ -3,3 +3,11 @@ package gg.flyte.twilight.gson
 import com.google.gson.GsonBuilder
 
 val GSON = GsonBuilder().setPrettyPrinting().create()!!
+
+/**
+ * Converts an object to its JSON representation using Google's Gson library.
+ *
+ * @receiver The object to be converted to JSON.
+ * @return The JSON representation of the object.
+ */
+fun Any.toJson() = GSON.toJson(this)
