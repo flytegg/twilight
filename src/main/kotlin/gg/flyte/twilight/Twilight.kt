e7041643104e297a29ca998914f2b36fa4dc64a0
Josh <jshbker@gmail.com>
2023-11-05 04:46:55 +0000

    feat(twilight-builder): add default unit values to prevent needing to open body

--- src/main/kotlin/gg/flyte/twilight/Twilight.kt
+++ src/main/kotlin/gg/flyte/twilight/Twilight.kt
@@ -3,18 +3,15 @@ package gg.flyte.twilight
 import gg.flyte.twilight.data.MongoDB
 import gg.flyte.twilight.data.service.NameCacheService
 import gg.flyte.twilight.environment.Environment
-import gg.flyte.twilight.event.custom.interact.listener.InteractEventListener
-import gg.flyte.twilight.event.customEvents
+import gg.flyte.twilight.event.customEventListeners
 import gg.flyte.twilight.extension.applyForEach
-import gg.flyte.twilight.inventory.GUIListener
-import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Twilight(javaPlugin: JavaPlugin) {
 
     init {
         plugin = javaPlugin
-        run { customEvents }
+        run { customEventListeners }
     }
 
     companion object {
@@ -22,17 +19,17 @@ class Twilight(javaPlugin: JavaPlugin) {
         var usingEnv = false
     }
 
-    fun env(init: Environment.Settings.() -> Unit) {
+    fun env(init: Environment.Settings.() -> Unit = {}) {
         usingEnv = true
         Environment.env(Environment.Settings().apply(init))
     }
 
-    fun mongo(init: MongoDB.Settings.() -> Unit) = MongoDB.mongo(MongoDB.Settings().apply(init))
+    fun mongo(init: MongoDB.Settings.() -> Unit = {}) = MongoDB.mongo(MongoDB.Settings().apply(init))
 
-    fun nameCache(init: NameCacheService.Settings.() -> Unit) = NameCacheService.nameCache(NameCacheService.Settings().apply(init))
+    fun nameCache(init: NameCacheService.Settings.() -> Unit = {}) = NameCacheService.nameCache(NameCacheService.Settings().apply(init))
 
-    fun terminate() = customEvents.applyForEach { unregister() }
+    fun terminate() = customEventListeners.applyForEach { unregister() }
 
 }
 
-fun twilight(plugin: JavaPlugin, init: Twilight.() -> Unit): Twilight = Twilight(plugin).apply(init)
+fun twilight(plugin: JavaPlugin, init: Twilight.() -> Unit = {}): Twilight = Twilight(plugin).apply(init)
