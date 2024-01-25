f96a99b037b1c427fd0f9e5f39618f0957efae23
joshbker <43449531+joshbker@users.noreply.github.com>
2024-01-25 03:34:40 +0000

    feat: start twilight packet

---
+++ src/main/kotlin/gg/flyte/twilight/network/packet/TwilightPacket.kt
@@ -0,0 +1,9 @@
+package gg.flyte.twilight.network.packet
+
+import java.util.*
+
+class TwilightPacket(
+    val id: UUID = UUID.randomUUID(),
+
+) {
+}
---
+++ src/main/kotlin/gg/flyte/twilight/network/packet/TwilightPacketHandler.kt
@@ -0,0 +1,7 @@
+package gg.flyte.twilight.network.packet
+
+open class TwilightPacketHandler {
+    fun handle(packet: TwilightPacket) {
+
+    }
+}
