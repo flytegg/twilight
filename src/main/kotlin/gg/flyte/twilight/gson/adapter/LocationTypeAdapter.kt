ae3d9f00df7948cd0c1559a8391c7ffc986eef68
Josh <43449531+joshbker@users.noreply.github.com>
2024-02-13 18:17:46 +0000

    feat: (untested) modify LocationTypeAdapter to use new TwilightTypeAdapter

---
+++ src/main/kotlin/gg/flyte/twilight/gson/adapter/LocationTypeAdapter.kt
@@ -0,0 +1,42 @@
+package gg.flyte.twilight.gson.adapter
+
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
+import gg.flyte.twilight.gson.TwilightTypeAdapter
+import org.bukkit.Bukkit
+import org.bukkit.Location
+
+class LocationTypeAdapter(override var allowNull: Boolean = false) : TwilightTypeAdapter<Location>(allowNull) {
+
+    override fun writeSafe(writer: JsonWriter, instance: Location) {
+        require(!allowNull) { "Location cannot be null." }
+        writer.apply {
+            beginObject()
+            property("world", instance.world!!.name)
+            property("x", instance.x)
+            property("y", instance.y)
+            property("z", instance.z)
+            property("yaw", instance.yaw)
+            property("pitch", instance.pitch)
+            endObject()
+        }
+    }
+
+    override fun read(reader: JsonReader): Location = with(reader) {
+        val location = Location(null, 0.0, 0.0, 0.0)
+        beginObject()
+        while (hasNext()) {
+            when (nextName()) {
+                "world" -> location.world = Bukkit.getWorld(nextString())
+                "x" -> location.x = nextDouble()
+                "y" -> location.y = nextDouble()
+                "z" -> location.z = nextDouble()
+                "yaw" -> location.yaw = nextDouble().toFloat()
+                "pitch" -> location.pitch = nextDouble().toFloat()
+            }
+        }
+        endObject()
+        location
+    }
+
+}
