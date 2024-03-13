7f854497610262da52e674df205fe648a3a41253
Josh <43449531+joshbker@users.noreply.github.com>
2024-03-13 00:21:51 +0000

    feat: add software boolean convenience extension methods to Bukkit Server

---
+++ src/main/kotlin/gg/flyte/twilight/extension/Server.kt
@@ -0,0 +1,8 @@
+package gg.flyte.twilight.extension
+
+import gg.flyte.twilight.server.ServerSoftware
+import org.bukkit.Server
+
+fun Server.isPaper(): Boolean = ServerSoftware.isPaper()
+fun Server.isSpigot(): Boolean = ServerSoftware.isSpigot()
+fun Server.isCraftBukkit(): Boolean = ServerSoftware.isCraftBukkit()
