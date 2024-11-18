b093f41c271840e9fb37a2fb271554e6e097280c
SamuSonoIo <samusonoio24@gmail.com>
2024-11-18 21:23:06 +0100

    Scoreboard as an object

--- src/main/kotlin/gg/flyte/twilight/scoreboards/Scoreboard.kt
+++ src/main/kotlin/gg/flyte/twilight/scoreboards/Scoreboard.kt
@@ -10,44 +10,47 @@ import java.util.UUID
 /**
  * Create a static sidebar for multiple players
  */
-fun createStaticSidebar(players: MutableList<UUID>, title: String, lines: MutableMap<String, Int>) {
-    val board = Bukkit.getScoreboardManager().newScoreboard
 
-    val obj = board.registerNewObjective("title", "dummy")
-    obj.displayName(Component.text(title))
-    obj.displaySlot = DisplaySlot.SIDEBAR
+object Scoreboard {
+    fun createStaticSidebar(players: MutableList<UUID>, title: String, lines: MutableMap<String, Int>) {
+        val board = Bukkit.getScoreboardManager().newScoreboard
 
-    lines.forEach { (text, score) ->
-        val team = board.registerNewTeam("line_${score}")
-        val entry = "§${score}"
+        val obj = board.registerNewObjective("title", "dummy")
+        obj.displayName(Component.text(title))
+        obj.displaySlot = DisplaySlot.SIDEBAR
 
-        team.prefix(Component.text(text))
-        team.addEntry(entry)
+        lines.forEach { (text, score) ->
+            val team = board.registerNewTeam("line_${score}")
+            val entry = "§${score}"
 
-        obj.getScore(entry).score = score
-    }
+            team.prefix(Component.text(text))
+            team.addEntry(entry)
 
-    players.forEach { uuid ->
-        val player = Bukkit.getPlayer(uuid)
-        player?.scoreboard = board
-        player?.let {
-            ScoreboardSaver.add(uuid, board)
+            obj.getScore(entry).score = score
+        }
+
+        players.forEach { uuid ->
+            val player = Bukkit.getPlayer(uuid)
+            player?.scoreboard = board
+            player?.let {
+                ScoreboardSaver.add(uuid, board)
+            }
         }
     }
-}
 
-/**
- * Remove scoreboard from a player
- */
-fun removeScoreboard(uuid: UUID) {
-    val player = Bukkit.getPlayer(uuid)
-    player?.scoreboard = Bukkit.getScoreboardManager().mainScoreboard
-    ScoreboardSaver.remove(uuid)
-}
+    /**
+     * Remove scoreboard from a player
+     */
+    fun removeScoreboard(uuid: UUID) {
+        val player = Bukkit.getPlayer(uuid)
+        player?.scoreboard = Bukkit.getScoreboardManager().mainScoreboard
+        ScoreboardSaver.remove(uuid)
+    }
 
-/**
- * Clear all active scoreboards
- */
-fun clearBoards() {
-    ScoreboardSaver.clear()
+    /**
+     * Clear all active scoreboards
+     */
+    fun clearBoards() {
+        ScoreboardSaver.clear()
+    }
 }
