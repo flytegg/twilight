c7fa15424482f6798ba637d3f1de5bca6b4ccc78
SamuSonoIo <samusonoio24@gmail.com>
2024-12-17 12:53:47 +0100

    Minimessage implementation

--- src/main/kotlin/gg/flyte/twilight/scoreboard/TwilightScoreboard.kt
+++ src/main/kotlin/gg/flyte/twilight/scoreboard/TwilightScoreboard.kt
@@ -1,6 +1,6 @@
 package gg.flyte.twilight.scoreboard
 
-import net.kyori.adventure.text.Component
+import gg.flyte.twilight.string.toMini
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
@@ -15,11 +15,6 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
 
     private var updateTask: BukkitTask? = null
 
-    private data class LineUpdate(
-        val initialLine: Component,
-        val updateFunction: () -> Component
-    )
-
     init {
         objective.displaySlot = DisplaySlot.SIDEBAR
     }
@@ -28,16 +23,16 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
      * @param title the displayname of the scoreboard
      * Used for setting the displayname of a scoreboard
      */
-    fun title(title: Component) {
-        objective.displayName(title)
+    fun title(title: String) {
+        objective.displayName(title.toMini())
     }
 
     /**
-     * @param text the component you want to use at that line
+     * @param text the string you want to use at that line
      * @param score the score you want to set
      * Used to change a specific line.
      */
-    fun set(text: Component, score: Int) {
+    fun set(text: String, score: Int) {
         scoreboard.entries.filter {
             objective.getScore(it).score == score
         }.forEach {
@@ -46,21 +41,22 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
 
         val uniqueEntry = " ".repeat(score)
         val team = scoreboard.registerNewTeam(uniqueEntry)
-        team.prefix(text)
+        team.prefix(text.toMini())
         team.addEntry(uniqueEntry)
 
         objective.getScore(uniqueEntry).score = score
     }
+
     /**
-     * @param lines actual components that you want in your scoreboard
+     * @param lines actual strings that you want in your scoreboard
      * Used to add how many lines you want to the scoreboard
      */
-    fun setAll(vararg lines: Component) {
+    fun setAll(vararg lines: String) {
         clearLines()
         lines.withIndex().forEach { (index, line) ->
             val uniqueEntry = "§${lines.size - index}"
             val team = scoreboard.registerNewTeam(uniqueEntry)
-            team.prefix(line)
+            team.prefix(line.toMini())
             team.addEntry(uniqueEntry)
             objective.getScore(uniqueEntry).score = lines.size - index
         }
@@ -68,13 +64,13 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
 
     /**
      * @param score the number of the line you want to get.
-     * Return a component at a specific line.
+     * Return a String at a specific line.
      */
-    fun get(score: Int): Component? {
+    fun get(score: Int): String? {
         return scoreboard.entries.find {
             objective.getScore(it).score == score
         }?.let { entry ->
-            scoreboard.getTeam(entry)?.prefix()
+            scoreboard.getTeam(entry)?.prefix()?.toString()
         }
     }
 
@@ -110,7 +106,6 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
         scoreboard.objectives.forEach { it.unregister() }
     }
 
-
     /**
      * @param player the player you want to assign the scoreboard to
      * Assign the scoreboard to a player (Use a for Loop if you want to assign this to multiple players)
@@ -123,20 +118,20 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
      * @param player the player you want to remove the scoreboard to
      * Assign the scoreboard to a player (Use a for Loop if you want to assign this to multiple players)
      */
-    fun removeTo(player: Player) {
+    fun removeFrom(player: Player) {
         player.scoreboard = Bukkit.getScoreboardManager().mainScoreboard
     }
 
     /**
      * @param updateInterval the refresh rate of the scoreboard (In Ticks)
-     * @param lines dynamic components to display, with null values preserving existing lines
+     * @param lines dynamic strings to display, with null values preserving existing lines
      * Used to create dynamic scoreboard with partial updates
      */
-    fun updateLines(updateInterval: Long, vararg lines: Component?) {
+    fun updateLines(updateInterval: Long, vararg lines: String?) {
         updateTask?.cancel()
 
         val currentLines = lines.mapIndexed { index, line ->
-            line ?: get(lines.size - index) ?: Component.empty()
+            line ?: get(lines.size - index) ?: ""
         }
 
         updateTask = object : BukkitRunnable() {
@@ -146,7 +141,7 @@ class TwilightScoreboard(private val plugin: JavaPlugin) {
                     val uniqueEntry = "§${currentLines.size - index}"
                     scoreboard.getTeam(uniqueEntry)?.unregister()
                     val team = scoreboard.registerNewTeam(uniqueEntry)
-                    team.prefix(line)
+                    team.prefix(line.toMini())
                     team.addEntry(uniqueEntry)
                     objective.getScore(uniqueEntry).score = currentLines.size - index
                 }
