d9a5304039f2a2d48e158ad835e4604dfa076164
Josh <43449531+joshbker@users.noreply.github.com>
2025-01-10 13:53:39 +0000

    fix(gson): add expected bukkit class name

--- src/main/kotlin/gg/flyte/twilight/gson/adapter/ConfigurationSerializationTypeAdapter.kt
+++ src/main/kotlin/gg/flyte/twilight/gson/adapter/ConfigurationSerializationTypeAdapter.kt
@@ -10,13 +10,13 @@ import org.bukkit.configuration.serialization.ConfigurationSerialization
 class ConfigurationSerializationTypeAdapter<T : ConfigurationSerializable>(override var allowNull: Boolean = false) : TwilightTypeAdapter<T>(allowNull) {
 
     override fun writeSafe(writer: JsonWriter, instance: T) {
-        writer.jsonValue(defaultGson.toJson(instance.serialize()))
+        writer.jsonValue(defaultGson.toJson(instance.serialize().toMutableMap().apply { put("==", instance.javaClass.name) }))
     }
 
     @Suppress("unchecked_cast")
     override fun read(reader: JsonReader): T {
         val json = defaultGson.fromJson(JsonParser.parseReader(reader).asJsonObject, Map::class.java) as Map<String, Any>
-        return ConfigurationSerialization.deserializeObject(json) as? T ?: throw IllegalStateException("Failed to deserialize object")
+        return ConfigurationSerialization.deserializeObject(json) as? T ?: throw IllegalStateException("Failed to deserialize object: $json")
     }
 
 }
